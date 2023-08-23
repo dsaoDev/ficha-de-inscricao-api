ce3e38170834e3de62b21cf527f3f43468c71484
dsaoDev <davissgt1@hotmail.com>
2023-08-23 13:30:20 -0300

    Projeto criado -First commit

---
+++ src/main/java/com/dsaoDev/FichadeInscricao/FichaDeInscricaoApplication.java
@@ -0,0 +1,13 @@
+package com.dsaoDev.FichadeInscricao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FichaDeInscricaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FichaDeInscricaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsaoDev/FichadeInscricao/FichaDeInscricaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsaoDev.FichadeInscricao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FichaDeInscricaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
